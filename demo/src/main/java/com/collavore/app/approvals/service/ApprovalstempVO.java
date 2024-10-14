faaa038bcf36e082c889868e8607668b55d16248
Your Name <you@example.com>
2024-10-14 14:43:41 +0900

    전자결재 VO, mapper, service, impl

--- demo/src/main/java/com/collavore/app/approvals/service/ApprovalstempVO.java
+++ demo/src/main/java/com/collavore/app/approvals/service/ApprovalstempVO.java
@@ -5,10 +5,11 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class ApprovalstempVO { //전자결재 템플릿 테이블
+public class ApprovalstempVO {//전자결재 템플릿 테이블
 		//필드
 	private Integer eatNo; 	//템플릿 번호
 	private String title;  	//템플릿 제목
 	private String content; //템플릿 내용
 	private Date regDate;	//템플릿 생선 날짜
 }
+//1번
