454af0d1c50aa269cb84ab10e623f23c0f8b2552
admin <admin@YD02-20>
2024-10-14 17:31:07 +0900

    1014 프로젝트 리스트 출력

--- demo/src/main/java/com/collavore/app/project/service/PjService.java
+++ demo/src/main/java/com/collavore/app/project/service/PjService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public interface PjService {
+	//프로젝트현황 리스트 
+	public List<ProjectVO> projectList();
 	
 	
 }
--- demo/src/main/java/com/collavore/app/project/service/impl/ProjectServiceImpl.java
+++ demo/src/main/java/com/collavore/app/project/service/impl/ProjectServiceImpl.java
@@ -1,11 +1,27 @@
 package com.collavore.app.project.service.impl;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.collavore.app.project.mapper.ProjectMapper;
 import com.collavore.app.project.service.PjService;
+import com.collavore.app.project.service.ProjectVO;
 
 @Service	//AOP => @Transcational
 public class ProjectServiceImpl implements PjService{
+	private ProjectMapper projectMapper;
+	
+	@Autowired // 생성자 주입
+	public ProjectServiceImpl(ProjectMapper projectMapper) {
+		this.projectMapper = projectMapper;
+	}
+	
+	@Override
+	public List<ProjectVO> projectList() {
+		return projectMapper.selectProjectAll();
+	}
 
 }
