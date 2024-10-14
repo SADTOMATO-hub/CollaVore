40c977f6ffab079cc07f0036d9a730c530c4af42
wns3915 <wns3915@naver.com>
2024-10-14 12:40:34 +0900

    게시판관리 초기설정

---
+++ demo/src/main/java/com/collavore/app/board/mapper/BodsMapper.java
@@ -0,0 +1,16 @@
+package com.collavore.app.board.mapper;
+
+public interface BodsMapper {
+	// 전체조회
+	
+	// 단건조회
+	
+	// 게시판 목록 조회
+	
+	// 등록
+	
+	// 수정
+	
+	// 삭제
+	
+}
---
+++ demo/src/main/java/com/collavore/app/board/service/impl/BodsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.collavore.app.board.service.impl;
+
+import com.collavore.app.board.service.BodsService;
+
+public class BodsServiceImpl implements BodsService {
+
+}
