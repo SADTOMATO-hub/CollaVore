8ca64922f035245f72c7fc57f4f85f8f4cdac701
ksh <iiilllu2@naver.com>
2024-10-15 17:38:28 +0900

    1015 5시38분 김수호 커밋

--- demo/src/main/java/com/collavore/app/cals/service/SchsVO.java
+++ demo/src/main/java/com/collavore/app/cals/service/SchsVO.java
@@ -9,11 +9,10 @@ import lombok.Data;
 
 @Builder
 public class SchsVO {
-	private Integer schNo;
-	private String title;
-	private Date startDate;
-	private Date endDate;
-	private Integer calNo;
-	private String isAlarm;
-	
+	private Integer schNo;  // 자동 생성되는 번호
+    private String title;   // 일정 제목
+    private Date startDate; // 시작 날짜
+    private Date endDate;   // 종료 날짜
+    private Integer calNo;  // 캘린더 번호
+    private String isAlarm; // 알림 여부 (예/아니오)
 }
