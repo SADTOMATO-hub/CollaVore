33d1944511654ef62a6b099aced965b19ed4d2b7
HwangSangSeob <45357279+HwangSangSub@users.noreply.github.com>
2024-10-18 09:02:16 +0900

    플러터API 기능 추가

--- demo/src/main/java/com/collavore/app/api/flutter/FlutterController.java
+++ demo/src/main/java/com/collavore/app/api/flutter/FlutterController.java
@@ -16,18 +16,32 @@ import lombok.RequiredArgsConstructor;
 public class FlutterController {
 
 	private final FlutterService flutterService;
-
+	
+	// 로그인
 	@PostMapping("api/login")
 	@ResponseBody
 	public FlutterVO loginProcess2(@RequestBody FlutterVO flutterVO, Model model) {
 		FlutterVO result = flutterService.loginChk(flutterVO);
-		
-		if(result != null) {
-			
-		} else {
-			
-		}
-
 		return result != null ? result : new FlutterVO();
 	}
+	
+	// 아이디찾기
+	// 비밀번호찾기
+	// 일정목록조회
+	// 일정등록
+	// 일정상세
+	// 일정수정
+	// 프로젝트목록조회
+	// 프로젝트업무목록조회
+	// 프로젝트상세업무목록조회
+	// 프로젝트상세업무상세보기 
+	// 프로젝트상세업무상세보기-댓글조회
+	// 프로젝트상세업무상세보기-댓글등록
+	// 프로젝트상세업무상세보기-댓글삭제
+	// 전자결재문서목록조회
+	// 전자결재문서상세보기
+	// 전자결재문서승인,반려처리
+	// 회원정보수정
+	
+	
 }
