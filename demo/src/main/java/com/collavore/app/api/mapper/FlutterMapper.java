17d917835f95ed579e7729a2906e06a6673c36a4
HwangSangSeob <45357279+HwangSangSub@users.noreply.github.com>
2024-10-21 08:33:23 +0900

    2024-10-18일 플러터 작업

--- demo/src/main/java/com/collavore/app/api/mapper/FlutterMapper.java
+++ demo/src/main/java/com/collavore/app/api/mapper/FlutterMapper.java
@@ -6,4 +6,13 @@ public interface FlutterMapper {
 	// 로그인 시도
 	public FlutterVO selectUser(FlutterVO flutterVO);
 
+	// 아이디찾기
+	public String selectId(FlutterVO flutterVO);
+	
+	// 비밀번호찾기-계정유무확인
+	public int chkUser(FlutterVO flutterVO);
+	
+	// 비밀번호찾기-비밀번호변경
+	public int updatePassword(FlutterVO flutterVO);
+	
 }
