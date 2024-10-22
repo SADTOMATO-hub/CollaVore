76c0cd6a067b6da9c2b45692634aeaff7f213e35
admin <admin@YD2-05>
2024-10-22 17:41:38 +0900

    1022 사원 마이페이지 완, 관리자 사원등록완(정규식 추가 및, 수정, 삭제 예정)

--- demo/src/main/java/com/collavore/app/hrm/service/HrmVO.java
+++ demo/src/main/java/com/collavore/app/hrm/service/HrmVO.java
@@ -2,6 +2,8 @@ package com.collavore.app.hrm.service;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -14,8 +16,11 @@ public class HrmVO {
    private String address;       // 주소
    private String workType;      // 근무형태
    private String info;          // 나의 소개글
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
    private Date joinDate;        // 입사일
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
    private Date leaveDate;       // 퇴사일
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
    private Date regDate;         // 등록일
    private String gitToken;      // 개별토큰
    private String img;           // 이미지
@@ -24,16 +29,19 @@ public class HrmVO {
    private String deptName;      // 부서명
    private Integer mgrNo;        // 부서장 사원번호
    private Integer parentDeptNo; // 상위부서번호
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
    private Date deptDate;        // 부서 등록일
    private Integer empCnt;       // 사원수
    
    private Integer jobNo;        // 직무번호
    private String jobName;       // 직무명
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
    private Date jobDate;         // 직무 등록일
    
    private Integer posiNo;       // 직위번호
    private String posiName;      // 직위명
    private Integer grade;        // 직위등급
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
    private Date posiDate;        // 직위 등록일
    
 }
--- demo/src/main/java/com/collavore/app/hrm/service/MemberService.java
+++ demo/src/main/java/com/collavore/app/hrm/service/MemberService.java
@@ -4,24 +4,28 @@ import java.util.List;
 
 public interface MemberService {
 
-    // 사원 전체 조회 (관리자)
-    List<HrmVO> memberList();
+    // 로그인 인증 처리 (이메일과 비밀번호 비교)
+    boolean authenticate(String email, String password); // 이메일과 비밀번호 인증
 
     // 사원 단건 조회
     HrmVO memberInfo(HrmVO hrmVO);
 
-    // 사원 등록
-    int memberInsert(HrmVO hrmVO);
-
     // 사원 수정
-    int memberUpdate(HrmVO hrmVO);  // 사원 정보 수정 메서드
-
-    // 사원 삭제
-    int memberDelete(int empNo);  // 사원 삭제 메서드
+    int memberUpdate(HrmVO hrmVO); // 사원 정보 수정 메서드
 
     // 이메일을 기반으로 사용자 정보 조회
     HrmVO findByEmail(String email);
 
-    // 로그인 인증 처리 (이메일과 비밀번호 비교)
-    boolean authenticate(String email, String password);  // 이메일과 비밀번호 인증
+    // 관리자 영역 ─────────────────────────────────────────
+    // 사원 전체 조회
+    List<HrmVO> selectMemberAll();
+
+    // 사원 등록
+    int insertMember(HrmVO hrmVO);
+
+    // 사번 자동 생성 (정수형으로 변경)
+    Integer generateEmpNo();
+
+    // 사원 삭제 (사번을 정수형으로 처리)
+    int deleteMember(int empNo); 
 }
