20be1bd8228039c0ba86ceab13af33f99cd335ec
HwangSangSeob <45357279+HwangSangSub@users.noreply.github.com>
2024-10-24 18:34:52 +0900

    플러터 API 웹주소 시큐리티에서 모두 허용으로 변경

--- demo/src/main/java/com/collavore/app/security/config/SpringSecurityConfig.java
+++ demo/src/main/java/com/collavore/app/security/config/SpringSecurityConfig.java
@@ -41,7 +41,7 @@ public class SpringSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/assets/**", "/dist/**", "/smarteditor/**", "/").permitAll()
+                .requestMatchers("/assets/**", "/dist/**", "/smarteditor/**", "/api/**", "/").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
