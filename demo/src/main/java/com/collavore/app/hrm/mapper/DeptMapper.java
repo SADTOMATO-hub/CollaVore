5d6b0db15ec2855739275c2f264e0957c4f83444
admin <admin@YD2-05>
2024-10-28 17:54:12 +0900

    1028 관리자 - 조직도 98% 완성

--- demo/src/main/java/com/collavore/app/hrm/mapper/DeptMapper.java
+++ demo/src/main/java/com/collavore/app/hrm/mapper/DeptMapper.java
@@ -8,15 +8,24 @@ import com.collavore.app.hrm.service.HrmVO;
 
 public interface DeptMapper {
 
-    // 부서 정보 삽입
-    public int insertDept(HrmVO hrmVO);
+	// 부서 정보 삽입
+	public int insertDept(HrmVO hrmVO);
 
-    // 부서 정보 업데이트
-    public int updateDept(HrmVO hrmVO);
+	// 부서 정보 업데이트
+	public int updateDept(HrmVO hrmVO);
 
-    // 부서 삭제
-    public int deleteDept(@Param("deptNo") Integer deptNo);
+	// 부서 삭제
+	public int deleteDept(@Param("deptNo") Integer deptNo);
 
-    // 기존 부서 목록 조회
-    public List<HrmVO> selectDeptList();
+	// 부서에 속한 사원 수를 확인하는 메서드
+	int countEmployeesInDept(@Param("deptNo") Integer deptNo);
+
+	// 기존 부서 목록 조회
+	public List<HrmVO> selectDeptList();
+
+	// 부서별 사원 목록 조회
+	List<HrmVO> selectEmployeesByDept(@Param("deptNo") Integer deptNo);
+
+	// 부서장 업데이트
+	int updateManager(@Param("deptNo") Integer deptNo, @Param("empNo") Integer empNo);
 }
