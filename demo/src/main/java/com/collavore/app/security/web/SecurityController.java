41951a1b9a8a91c6dacfaa6acb09daa2739025be
HwangSangSeob <45357279+HwangSangSub@users.noreply.github.com>
2024-11-05 10:02:26 +0900

    로그인 오류 수정

--- demo/src/main/java/com/collavore/app/security/web/SecurityController.java
+++ demo/src/main/java/com/collavore/app/security/web/SecurityController.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.collavore.app.security.service.UserVO;
 import com.collavore.app.security.service.impl.UserDetailsService;
 
 import jakarta.servlet.http.HttpSession;
@@ -17,20 +15,24 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class SecurityController {
 
-    private final UserDetailsService userDetailsService;
+	private final UserDetailsService userDetailsService;
 
-    @ModelAttribute
-    public void addAttributes(Model model) {
-        model.addAttribute("sidemenu", "default_sidebar");
-    }
+	@ModelAttribute
+	public void addAttributes(Model model) {
+		model.addAttribute("sidemenu", "default_sidebar");
+	}
 
-    @GetMapping("/")
-    public String all() {
-        return "login";
-    }
+	@GetMapping("/")
+	public String all() {
+		return "login";
+	}
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
+	@GetMapping("/login")
+	public String logins(@RequestParam(value = "error", required = false) String error, HttpSession session,
+			Model model) {
+		if (error != null) {
+			model.addAttribute("loginError", "아이디 또는 비밀번호가 올바르지 않습니다.");
+		}
+		return "login";
+	}
 }
